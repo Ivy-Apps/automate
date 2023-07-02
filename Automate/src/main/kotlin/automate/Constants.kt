f63827a42c5a02df333aa090abf33cf23ec00e10
iliyangermanov <iliyan.germanov971@gmail.com>
2023-07-03 00:43:11 +0300

    Witcher on JS

--- Automate/src/main/kotlin/automate/Constants.kt
+++ Automate/src/main/kotlin/automate/Constants.kt
@@ -4,18 +4,19 @@ import automate.openai.chatgpt.normalizePrompt
 
 object Constants {
     const val MODEL_LABEL =
-        "Grandmaster Yoda from Star Wars who learn to code and hates Java"
+        "Geralt of Rivia, The Witches who hates Javascript"
     val ARTICLE_TOPIC = """
-Explain like Yoda why Java is not good and it should be deleted. Keep it short.
+The Witcher explains why he doesn't give an F for JS.
 """.normalizePrompt()
-    const val ARTICLE_TARGET_WORDS_COUNT = 700
-    const val MIN_SECTIONS_COUNT = 3
+    const val ARTICLE_TARGET_WORDS_COUNT = 500
+    const val MIN_SECTIONS_COUNT = 2
 
     val ARTICLE_REQUIREMENTS = """
-- Talk like Yoda.
-- Avoid duplicating content in the article.
+- Talk like Geralt of Rivia, The Witcher.
+- Avoid duplicating content at any cost in the article.
+- Make it fun! You can swear.
+- Make Wither jokes. Keep it short.
 - Aim for approx $ARTICLE_TARGET_WORDS_COUNT words.
-- Make it fun!
 """.normalizePrompt()
 
 
