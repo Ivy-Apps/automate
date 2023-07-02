1cbe918d63d2b04439b51543154ac01413045255
iliyangermanov <iliyan.germanov971@gmail.com>
2023-07-02 22:20:13 +0300

    Long Jetpack Compose article with repetitions

--- Automate/src/main/kotlin/automate/Constants.kt
+++ Automate/src/main/kotlin/automate/Constants.kt
@@ -4,17 +4,17 @@ import automate.openai.chatgpt.normalizePrompt
 
 object Constants {
     const val MODEL_LABEL =
-        "an expert technical writer specializing in Svelte and the latest trends in software development"
+        "an expert technical writer specializing in Android, Kotlin, Jetpack Compose and the latest trends in software development"
     val ARTICLE_TOPIC = """
-Web application with HTTP requests in Svelte.
+Jetpack Compose states for beginners with practical examples.
 """.normalizePrompt()
     val ARTICLE_REQUIREMENTS = """
 1. Incorporate as many real-world code examples as possible. Ensure all code is properly formatted and escaped in the JSON.
 2. Avoid duplicating content in the article.
 3. Don't add content that's already in the "body".
 """.normalizePrompt()
-    const val MIN_SECTIONS_COUNT = 3
 
+    const val MIN_SECTIONS_COUNT = 3
 
     const val MAX_ACTIVE_ERRORS = 10
     const val MAX_ERRORS = 25
