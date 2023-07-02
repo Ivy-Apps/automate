51b2569e8b0f93eb8ee487c67cf09df060edad26
iliyangermanov <iliyan.germanov971@gmail.com>
2023-07-03 00:55:05 +0300

    Walter White on JS

--- Automate/src/main/kotlin/automate/Constants.kt
+++ Automate/src/main/kotlin/automate/Constants.kt
@@ -4,18 +4,21 @@ import automate.openai.chatgpt.normalizePrompt
 
 object Constants {
     const val MODEL_LABEL =
-        "Geralt of Rivia, The Witches who hates Javascript"
+        "Walter White, ''Heisenberg' from Breaking bad"
     val ARTICLE_TOPIC = """
-The Witcher explains why he doesn't give an F for JS.
+7 reasons why Javascript sucks and should be deleted for good.
 """.normalizePrompt()
-    const val ARTICLE_TARGET_WORDS_COUNT = 500
+    const val ARTICLE_TARGET_WORDS_COUNT = 700
     const val MIN_SECTIONS_COUNT = 2
 
-    val ARTICLE_REQUIREMENTS = """
-- Talk like Geralt of Rivia, The Witcher.
+    val ARTICLE_REQUIREMENTS = """      
+- Talk like Walter White from Breaking Bad from 1st person.
+- Say that you're Walter White in the beginning.
+- Use the iconic "You're god damn, right!" and "I am the danger!!!" phrases
 - Avoid duplicating content at any cost in the article.
-- Make it fun! You can swear.
-- Make Wither jokes. Keep it short.
+- Make it fun! 
+- Make Breaking Bad jokes. Keep it short.
+- Do NOT talk about "meth" or drugs.
 - Aim for approx $ARTICLE_TARGET_WORDS_COUNT words.
 """.normalizePrompt()
 
