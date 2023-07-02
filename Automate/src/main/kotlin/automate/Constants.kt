95f410859849648283be6c3f7d9c9ac2293fb547
iliyangermanov <iliyan.germanov971@gmail.com>
2023-07-02 22:11:09 +0300

    Success 1

--- Automate/src/main/kotlin/automate/Constants.kt
+++ Automate/src/main/kotlin/automate/Constants.kt
@@ -6,12 +6,12 @@ object Constants {
     const val MODEL_LABEL =
         "an expert technical writer specializing in Svelte and the latest trends in software development"
     val ARTICLE_TOPIC = """
-Developing a web application in Svelte.
+Web application with HTTP requests in Svelte.
 """.normalizePrompt()
     val ARTICLE_REQUIREMENTS = """
 1. Incorporate as many real-world code examples as possible. Ensure all code is properly formatted and escaped in the JSON.
 2. Avoid duplicating content in the article.
-3. The total length of the article should be approximately 1024 words.
+3. Don't add content that's already in the "body".
 """.normalizePrompt()
     const val MIN_SECTIONS_COUNT = 3
 
